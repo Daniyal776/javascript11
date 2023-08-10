bb105a279622413410dc313de78101add68f8d91
Daniyal776 <136301107+Daniyal776@users.noreply.github.com>
2023-08-09 22:04:34 -0700

    my javascript file is compeleted

---
+++ store.js
@@ -0,0 +1,84 @@
+//  QUESTION:01
+//var userName=prompt("Whats your Name");
+//console.log(userName);
+//if(userName){
+  //  alert('Hello'+" "+userName)
+//}
+//  QUESTION:02
+//let usreage =prompt("your age");
+// console.log(usreage);
+// //if(usreage>=18){
+//     alert('thank you'){
+// }
+// else{
+//     alert('sorry your age uncompelet')
+// }
+// //  QUESTION:03
+// let conformage=prompt('your conform age')
+// console.log(conformage);
+// if(conformage >=13 && conformage>=19){
+//     alert('you are teenger')
+// }else{
+//     alert('you are not teenger'
+//     )
+//}
+//  QUESTION:04
+//let  userheight =prompt(' height');
+//let width=prompt('width');
+////let areaheight* width
+//alert('area of rectanglen is'+ area)
+//console.log(height);
+
+
+
+//  QUESTION:05
+
+// let cottan=prompt('cottan  size');
+// console.log(cottan);
+// if(cottanMed >=10||cottanMed >=18|| lar 30 ){
+//     alert('onform size cottan')
+// }else{
+//     alert('ok Done ')
+// }
+
+//  QUESTION:06
+// let price=prompt('price');
+// let discount=prompt('Discount')
+// let discountedprice =price-discount
+// console.log('Discount amount');
+// document.write('price after disconut is'+ discountedprice)
+
+
+//  QUESTION:07
+// let length=prompt('length')
+// console.log(length);
+// if(length>=5.3 && length<=10){
+//     alert('your length compeleted')
+// }else{
+//     alert('your length not compeleted')
+// }
+
+//  QUESTION:08
+let country=prompt('you country')
+console.log(country);
+if(North||Shoth||East||west){
+    alert('countery is macth')
+}else{
+    alert('Does not macth country')
+}
+
+//  QUESTION:09
+
+
+
+//  QUESTION:10
+
+
+
+//  QUESTION:11
+
+
+
+
+
+
